34babf191d316b080535dba162b36bb91527c3be
Babita Kumari <babitakumari.ky3007@gmail.com>
2025-03-01 11:50:16 +0530

    added Demo folder

---
+++ Demo/chaiaurDjango/chaiaurDjango/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    # DB OPERATIONS
+    # CALCULATION
+
+
+
+    return render(request, 'about.html')
+    # return HttpResponse("<h1> WELCOME TO ABOUT PAGE </h1>")
+
+def contact(request):
+    return HttpResponse("<h1> WELCOME TO CONTACT PAGE </h1>")
+
+def findPercentage(reqeust):
+    price=50
+    perc=5
+
+    ans=price*perc/100
+    return JsonResponse({
+        "price":price,
+        "perc":perc,
+        "ans":ans
+    })
