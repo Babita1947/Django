9c44eacf7c414c7749a894bafe1ad688dbb1047d
Babita Kumari <babitakumari.ky3007@gmail.com>
2025-03-18 09:56:11 +0530

    exploring forms in Django

--- Revision/Firstproject/chai/urls.py
+++ Revision/Firstproject/chai/urls.py
@@ -5,6 +5,6 @@ from . import views
 # localhost:8000/chai/order
 urlpatterns = [
     path('',views.all_chai, name='all_chai'),  # /chai/
-    path('<int:chai_id>/',views.chai_detail, name='chai_detail'), #/chai/<int>
-    
+    path('<int:chai_id>/',views.chai_detail, name='chai_detail'), # /chai/<int>
+    path('chai_stores/',views.chai_stores_view, name='chai_stores'),
 ]
--- Revision/Firstproject/chai/views.py
+++ Revision/Firstproject/chai/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import ChaiVarity
+from .models import ChaiVarity, Store
 from django.shortcuts import get_object_or_404
+from .forms import ChaiVarityForm
+
 
 # Create your views here.
 def all_chai(request):
@@ -10,3 +12,19 @@ def all_chai(request):
 def chai_detail(request, chai_id):
     chai = get_object_or_404(ChaiVarity, pk = chai_id)
     return render(request, 'chai/chai_detail.html', {'chai' : chai})
+
+def chai_stores_view(request):
+    stores = None
+    if request.method == 'POST':
+        form = ChaiVarityForm(request.POST) # Bring option data of chai_varity from forms.py
+        print(form)
+        if form.is_valid():
+            chai_varity = form.cleaned_data['chai_varity']# that 'chai_varity' coming from instance variable of forms.py 
+            print(chai_varity)
+            stores = Store.objects.filter(chai_varities = chai_varity)
+    else:
+        form = ChaiVarityForm()
+
+    
+    return render(request, 'chai/chai_stores.html', {'stores': stores, 'form': form})
+
